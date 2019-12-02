6c293dc8a52ec9d378f479c774f7d4d056af0b44
Thomas679 <49179886+Thomas679@users.noreply.github.com>
2019-12-02 15:08:53 -0500

    swap

--- src/test-sql.js
+++ src/test-sql.js
@@ -1,23 +1,32 @@
-var mysql = require('mysql');
 
-var con = mysql.createConnection(
-    {
-        host: 'triton.towson.edu',
-        user: 'dgargu1',
-        password:'Cosc*ad35',
-        port:'3360'
-    });
+    var mysql = require('mysql');
+function again()
+{
+    console.log('Work')
+}
+function callSQL()
+{
 
-    console.log("hmm")
+    var con = mysql.createConnection(
+        {
+            host: 'triton.towson.edu',
+            user: 'dgargu1',
+            password:'Cosc*ad35',
+            port:'3360'
+        });
     
-    con.connect(function(err)
-    {
-        if (err) throw err;
-        console.log("Connected!");
-        con.query('SELECT * FROM dgargu1db.login', function(err, result, fields)
+        console.log("hmm")
+        
+        con.connect(function(err)
         {
             if (err) throw err;
-            console.log(result);
+            console.log("Connected!");
+            con.query('SELECT * FROM dgargu1db.login', function(err, result, fields)
+            {
+                if (err) throw err;
+                console.log(result);
+            });
         });
-    });
-    con.close();
+        con.close();
+    
+}
