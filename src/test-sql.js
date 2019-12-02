47f2ebc5a0a93c2dba4cccbec7ebb9e7de892b07
DomGarguilo <dominic.garguilo@gmail.com>
2019-12-02 14:16:44 -0500

    test query

---
+++ src/test-sql.js
@@ -0,0 +1,22 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection(
+    {
+        host: 'triton.towson.edu',
+        user: 'dgargu1',
+        password:'Cosc*ad35',
+        port:'3360'
+    });
+
+    console.log("hmm")
+    
+    con.connect(function(err)
+    {
+        if (err) throw err;
+        console.log("Connected!");
+        con.query('SELECT * FROM dgargu1db.ingredient', function(err, result, fields)
+        {
+            if (err) throw err;
+            console.log(result);
+        });
+    });
